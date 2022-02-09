3693f5b52fb855458e0e8b53527111e1359e8700
ednoram <e.noramirian@gmail.com>
2022-02-09 18:36:28 +0400

    fix jwt expiration

--- src/utils/signJWT.ts
+++ src/utils/signJWT.ts
@@ -3,7 +3,11 @@ import jwt from "jsonwebtoken";
 import { TOKEN_EXPIRY_SECONDS, TOKEN_SECRET } from "@config";
 
 const signJWT = (_id: string, email: string): string => {
-  return jwt.sign({ email, _id, duration: TOKEN_EXPIRY_SECONDS }, TOKEN_SECRET);
+  return jwt.sign(
+    { email, _id, duration: TOKEN_EXPIRY_SECONDS },
+    TOKEN_SECRET,
+    { expiresIn: TOKEN_EXPIRY_SECONDS }
+  );
 };
 
 export default signJWT;
